68909bb0a357e8672c6f5eccd080a0c7bdd78727
norberts.jegers <norberts.jegers@gmail.com>
2021-11-19 23:30:38 +0200

    small refactoring

--- src/index.ts
+++ src/index.ts
@@ -1,79 +1,45 @@
-import { statSync, Stats, readdirSync } from 'fs';
-import * as path from 'path';
-import { writeFile, readFile, OutputFile, createOutputFile } from './io';
+import { File, readFiles, writeFiles } from './io';
 import SyntaxAnalyser from './analyser';
 
-function compileVM(input: string[]): string[] {
-    return [];
-}
+function compileFiles(files: File[], args: string[]) {
+    const compiledFiles: File[] = [];
 
-async function compile(
-    filePath: string,
-    fileName: string,
-    parsedPath: path.ParsedPath,
-    args: string[],
-    fromDir: boolean = false
-): Promise<OutputFile[]> {
-    const outputFiles: OutputFile[] = [];
+    for (const file of files) {
+        compiledFiles.push({
+            name: file.name,
+            extension: 'vm',
+            dir: file.dir,
+            data: [],
+        });
 
-    const input: string[] = await readFile(filePath);
-    outputFiles.push(createOutputFile(compileVM(input), fileName, parsedPath, 'vm', fromDir));
+        if (args.includes('--syntax-tree')) {
+            compiledFiles.push({
+                name: file.name,
+                extension: 'xml',
+                dir: file.dir,
+                data: SyntaxAnalyser.getClass(file.data),
+            });
+        }
 
-    if (args.includes('--syntax-tree')) {
-        outputFiles.push(createOutputFile(SyntaxAnalyser.getClass(input), fileName, parsedPath, 'xml', fromDir));
+        if (args.includes('--tokens')) {
+            compiledFiles.push({
+                name: file.name + 'T',
+                extension: 'xml',
+                dir: file.dir,
+                data: SyntaxAnalyser.getClassTokens(file.data),
+            });
+        }
     }
 
-    if (args.includes('--tokens')) {
-        outputFiles.push(
-            createOutputFile(
-                SyntaxAnalyser.getClassTokens(input),
-                fileName + 'T',
-                { ...parsedPath, name: parsedPath.name + 'T' },
-                'xml',
-                fromDir
-            )
-        );
-    }
-
-    return outputFiles;
+    return compiledFiles;
 }
 
 async function main(): Promise<void> {
     try {
         const args: string[] = process.argv.slice(2);
-        const providedPath: string = args[0];
-
-        if (!providedPath) {
-            throw new Error('Path argument is missing');
-        }
-
-        const parsedPath: path.ParsedPath = path.parse(providedPath);
-        const stats: Stats = statSync(providedPath);
-        let outputFiles: OutputFile[] = [];
-
-        if (parsedPath.ext === '.jack' && !stats.isDirectory()) {
-            outputFiles = await compile(providedPath, '', parsedPath, args);
-        } else if (parsedPath.ext === '' && stats.isDirectory() && readdirSync(providedPath).length > 0) {
-            const fileNames: string[] = readdirSync(providedPath).filter((f) => f.includes('.jack'));
-            for (const fileName of fileNames) {
-                outputFiles = [
-                    ...outputFiles,
-                    ...(await compile(
-                        `${providedPath}/${fileName}`,
-                        fileName.replace('.jack', ''),
-                        parsedPath,
-                        args,
-                        true
-                    )),
-                ];
-            }
-        } else {
-            throw new Error('Path argument is not valid (must be either a .jack file or an existing directory)');
-        }
-
-        for (const file of outputFiles) {
-            writeFile(file);
-        }
+        const files: File[] = await readFiles(args);
+        const compiledFiles: File[] = compileFiles(files, args);
+        writeFiles(compiledFiles);
 
         console.log('Compilation finished');
     } catch (e) {
--- src/io.ts
+++ src/io.ts
@@ -1,36 +1,12 @@
-import { createReadStream, createWriteStream, WriteStream } from 'fs';
+import { createReadStream, createWriteStream, WriteStream, Stats, statSync, readdirSync } from 'fs';
 import { createInterface } from 'readline';
 import * as path from 'path';
 
-export interface OutputFile {
+export interface File {
+    name: string;
+    extension: string;
+    dir: string;
     data: string[];
-    path: string;
-}
-
-export function createOutputFile(
-    data: string[],
-    fileName: string,
-    parsedPath: path.ParsedPath,
-    ext: string,
-    fromDir: boolean = false
-): OutputFile {
-    return {
-        data,
-        path:
-            parsedPath.dir.length > 0
-                ? fromDir
-                    ? `${parsedPath.dir}/${parsedPath.base}/${fileName}.${ext}`
-                    : `${parsedPath.dir}/${parsedPath.name}.${ext}`
-                : `${parsedPath.name}.${ext}`,
-    };
-}
-
-export function writeFile(file: OutputFile): void {
-    const wstream: WriteStream = createWriteStream(file.path);
-
-    for (const line of file.data) {
-        wstream.write(line + '\n');
-    }
 }
 
 export function readFile(filePath: string): Promise<string[]> {
@@ -48,4 +24,53 @@ export function readFile(filePath: string): Promise<string[]> {
             resolve(data);
         });
     });
+}
+
+export async function readFiles(args: string[]): Promise<File[]> {
+    const providedPath: string = args[0];
+
+    if (!providedPath) {
+        throw new Error('Path argument is missing');
+    }
+
+    const parsedPath: path.ParsedPath = path.parse(providedPath);
+    const stats: Stats = statSync(providedPath);
+    let files: File[] = [];
+
+    if (parsedPath.ext === '.jack' && !stats.isDirectory()) {
+        files.push({
+            name: parsedPath.name,
+            extension: null,
+            dir: parsedPath.dir,
+            data: await readFile(providedPath),
+        });
+    } else if (parsedPath.ext === '' && stats.isDirectory() && readdirSync(providedPath).length > 0) {
+        const fileNames: string[] = readdirSync(providedPath).filter((f) => f.includes('.jack'));
+        for (const fileName of fileNames) {
+            files.push({
+                name: fileName.replace('.jack', ''),
+                extension: null,
+                dir: `${parsedPath.dir}/${parsedPath.base}`,
+                data: await readFile(`${providedPath}/${fileName}`),
+            });
+        }
+    } else {
+        throw new Error('Path argument is not valid (must be either a .jack file or an existing directory)');
+    }
+
+    return files;
+}
+
+export function writeFile(file: File): void {
+    const wstream: WriteStream = createWriteStream(`${file.dir}/${file.name}.${file.extension}`);
+
+    for (const line of file.data) {
+        wstream.write(line + '\n');
+    }
+}
+
+export function writeFiles(files: File[]): void {
+    for (const file of files) {
+        writeFile(file);
+    }
 }
